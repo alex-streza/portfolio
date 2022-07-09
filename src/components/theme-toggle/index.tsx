b49b2003812065775c9d4679433e6857ec938a9a
alex-streza <alex.streza@okapistudio.com>
2022-07-09 21:09:07 +0300

    attempt fix dark mode glitch

--- src/components/theme-toggle/index.tsx
+++ src/components/theme-toggle/index.tsx
@@ -1,50 +1,52 @@
 import { useState, useEffect, useCallback } from 'react'
 
-const getIsDarkTheme = () => localStorage.getItem('dark') == 'dark'
+const getIsDarkTheme = () => localStorage.getItem('theme') === 'dark'
+
+const MoonIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="-4 -2 24 24"
+    width="32"
+    fill="currentColor"
+  >
+    <path d="M2 10c0 4.43 3.478 8 7.742 8 .658 0 1.302-.085 1.922-.248-2.996-2.2-4.896-5.786-4.896-9.752 0-2.09.527-4.095 1.489-5.853C4.699 2.863 2 6.097 2 10zm6.768-2c0 4.632 3.068 8.528 7.232 9.665A9.555 9.555 0 0 1 9.742 20C4.362 20 0 15.523 0 10S4.362 0 9.742 0c.868 0 1.71.117 2.511.335A10.086 10.086 0 0 0 8.768 8z"></path>
+  </svg>
+)
+
+const SunIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="-2 -2 24 24"
+    width="32"
+    fill="currentColor"
+  >
+    <path d="M10 13a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm0 2a5 5 0 1 1 0-10 5 5 0 0 1 0 10zm0-15a1 1 0 0 1 1 1v2a1 1 0 0 1-2 0V1a1 1 0 0 1 1-1zm0 16a1 1 0 0 1 1 1v2a1 1 0 0 1-2 0v-2a1 1 0 0 1 1-1zM1 9h2a1 1 0 1 1 0 2H1a1 1 0 0 1 0-2zm16 0h2a1 1 0 0 1 0 2h-2a1 1 0 0 1 0-2zm.071-6.071a1 1 0 0 1 0 1.414l-1.414 1.414a1 1 0 1 1-1.414-1.414l1.414-1.414a1 1 0 0 1 1.414 0zM5.757 14.243a1 1 0 0 1 0 1.414L4.343 17.07a1 1 0 1 1-1.414-1.414l1.414-1.414a1 1 0 0 1 1.414 0zM4.343 2.929l1.414 1.414a1 1 0 0 1-1.414 1.414L2.93 4.343A1 1 0 0 1 4.343 2.93zm11.314 11.314l1.414 1.414a1 1 0 0 1-1.414 1.414l-1.414-1.414a1 1 0 1 1 1.414-1.414z"></path>
+  </svg>
+)
 
 const ThemeToggle = () => {
-  const [isDark, setIsDark] = useState(getIsDarkTheme)
+  const [isDark, setIsDark] = useState(getIsDarkTheme ?? false)
+  const [isMounted, setIsMounted] = useState(false)
 
   const toggleTheme = useCallback(() => {
-    if (isDark) localStorage.setItem('dark', 'true')
-    else localStorage.setItem('dark', 'false')
-    setIsDark(!isDark)
+    if (isDark) localStorage.setItem('theme', 'light')
+    else localStorage.setItem('theme', 'dark')
 
     if (isDark) {
-      document.documentElement.classList.add('dark')
-    } else {
       document.documentElement.classList.remove('dark')
+    } else {
+      document.documentElement.classList.add('dark')
     }
+
+    setIsDark(!isDark)
   }, [isDark])
 
-  useEffect(() => {
-    const id = setTimeout(setIsDark(getIsDarkTheme()), 100)
+  useEffect(() => setIsMounted(true), [])
 
-    return () => clearTimeout(id)
-  }, [])
+  if (!isMounted) return <SunIcon />
 
   return (
-    <button onClick={toggleTheme}>
-      {isDark ? (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="-2 -2 24 24"
-          width="32"
-          fill="currentColor"
-        >
-          <path d="M10 13a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm0 2a5 5 0 1 1 0-10 5 5 0 0 1 0 10zm0-15a1 1 0 0 1 1 1v2a1 1 0 0 1-2 0V1a1 1 0 0 1 1-1zm0 16a1 1 0 0 1 1 1v2a1 1 0 0 1-2 0v-2a1 1 0 0 1 1-1zM1 9h2a1 1 0 1 1 0 2H1a1 1 0 0 1 0-2zm16 0h2a1 1 0 0 1 0 2h-2a1 1 0 0 1 0-2zm.071-6.071a1 1 0 0 1 0 1.414l-1.414 1.414a1 1 0 1 1-1.414-1.414l1.414-1.414a1 1 0 0 1 1.414 0zM5.757 14.243a1 1 0 0 1 0 1.414L4.343 17.07a1 1 0 1 1-1.414-1.414l1.414-1.414a1 1 0 0 1 1.414 0zM4.343 2.929l1.414 1.414a1 1 0 0 1-1.414 1.414L2.93 4.343A1 1 0 0 1 4.343 2.93zm11.314 11.314l1.414 1.414a1 1 0 0 1-1.414 1.414l-1.414-1.414a1 1 0 1 1 1.414-1.414z"></path>
-        </svg>
-      ) : (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="-4 -2 24 24"
-          width="32"
-          fill="currentColor"
-        >
-          <path d="M2 10c0 4.43 3.478 8 7.742 8 .658 0 1.302-.085 1.922-.248-2.996-2.2-4.896-5.786-4.896-9.752 0-2.09.527-4.095 1.489-5.853C4.699 2.863 2 6.097 2 10zm6.768-2c0 4.632 3.068 8.528 7.232 9.665A9.555 9.555 0 0 1 9.742 20C4.362 20 0 15.523 0 10S4.362 0 9.742 0c.868 0 1.71.117 2.511.335A10.086 10.086 0 0 0 8.768 8z"></path>
-        </svg>
-      )}
-    </button>
+    <button onClick={toggleTheme}>{isDark ? <MoonIcon /> : <SunIcon />}</button>
   )
 }
 
