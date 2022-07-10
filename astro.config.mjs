22f89a413a8d3fac5d261034589aaff1077bbe6e
alex-streza <alex.streza@okapistudio.com>
2022-07-10 12:45:56 +0300

    generate sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,23 @@ import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
 import { defineConfig } from 'astro/config'
 
+import sitemap from '@astrojs/sitemap'
+
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://www.blog.alexstreza.dev',
   vite: {
     ssr: {
       external: ['svgo'],
     },
   },
-  integrations: [react(), tailwind({ config: { applyBaseStyles: false } })],
+  integrations: [
+    react(),
+    tailwind({
+      config: {
+        applyBaseStyles: false,
+      },
+    }),
+    sitemap(),
+  ],
 })
