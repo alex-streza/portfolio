cae19fc3fa2584312f48dcffed49e0be6ec69c80
alex-streza <alex.streza@okapistudio.com>
2022-07-23 01:52:51 +0300

    add subscribe to medium feature

--- src/components/forms/NewsletterForm.tsx
+++ src/components/forms/NewsletterForm.tsx
@@ -1,13 +1,26 @@
 import Button from '@components/button/Button'
-import Input from '@components/input/Input'
+import Medium from '@components/icons/Medium'
+import Twitter from '@components/icons/Twitter'
 
 const NewsletterForm = () => {
   return (
-    <div className="flex gap-3 items-end">
-      <Input name="email" label="Email" placeholder="cool@cooler.com" />
-      <Button className="primary w-auto" disabled>
-        Subscribe
+    <div className="flex gap-3 items-center">
+      {/* <Input name="email" label="Email" placeholder="cool@cooler.com" /> */}
+      <Button className="primary w-auto">
+        <Medium />
+        <a
+          className="!text-gray-1000 no-underline font-semibold"
+          href="https://medium.com/subscribe/@alex.streza"
+        >
+          Subscribe on Medium
+        </a>
       </Button>
+      <a
+        href="https://twitter.com/alex_streza"
+        className="dark:text-white text-gray-1000 transition-all hover:rotate-12 hover:scale-105 hover:text-main dark:hover:text-main"
+      >
+        <Twitter width={40} />
+      </a>
     </div>
   )
 }
--- src/components/icons/Twitter.tsx
+++ src/components/icons/Twitter.tsx
@@ -1,8 +1,8 @@
-const Twitter = () => {
+const Twitter = ({ width = 32 }) => {
   return (
     <svg
-      width="32"
-      height="32"
+      width={width}
+      height={width}
       viewBox="0 0 32 32"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
